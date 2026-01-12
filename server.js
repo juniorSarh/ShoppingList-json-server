80f9691b2236b255b49532209b7b8a4242da4f67
juniorSarh <sabelohgumede@gmail.com>
2026-01-12 12:28:43 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const jsonServer = require("json-server");
+const { v4: uuidv4 } = require("uuid");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Middleware to generate random ID
+server.post("/users", (req, res, next) => {
+  req.body.id = uuidv4().slice(0, 4); // shorten to 4 chars
+  next();
+});
+
+server.post("/lists", (req, res, next) => {
+  req.body.id = uuidv4().slice(0, 4);
+  next();
+});
+
+servber.post("/items", (req, res, next) => {
+  req.body.id = uuidv4().slice(0, 4);
+  next();
+});
+
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running on port 3000 🚀");
+});
